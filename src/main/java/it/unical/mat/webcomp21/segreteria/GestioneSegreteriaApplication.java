646c03f7174c33897b8aff09d32a09c1192305f5
Simone Bilotta <Sispib1@gmail.com>
2020-10-30 08:59:12 +0100

    first commit

---
+++ src/main/java/it/unical/mat/webcomp21/segreteria/GestioneSegreteriaApplication.java
@@ -0,0 +1,13 @@
+package it.unical.mat.webcomp21.segreteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneSegreteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneSegreteriaApplication.class, args);
+	}
+
+}
